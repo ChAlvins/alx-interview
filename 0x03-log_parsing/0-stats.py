42907540d895c34b836386db6d907db0a87e1879
vpnchengo <charo.alvin@gmail.com>
2023-08-18 03:28:49 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,5 +1,40 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """module with a reading stdin and computes metrics"""
 import sys
 
 
+"""initialize dict to store status code counts to 0"""
+status_code_count = {
+        '200': 0, '301': 0, '400': 0, '401': 0,
+        '403': 0, '404': 0, '405': 0, '500': 0
+}
+
+total_size = 0
+counter = 0
+
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) >= 6:
+            status_code = line_list[-2]
+            file_size = int(line_list[-1])
+            if status_code in status_code_count:
+                status_code_count += 1
+            total_size += file_size
+            counter += 1
+
+        if counter == 10 or isinstance(err, KeyboardInterrupt):
+            print("File size: {}".format(total_size))
+            for key, value in sorted(status_code_count.items()):
+                if value != 0:
+                    print("{}: {}".format(key, value))
+            counter = 0
+
+except Exception as err:
+    pass
+
+finally:
+    print("File size: {}".format(total_size))
+    for key, value in sorted(status_code_count.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
