830035eca8e75ac825ee206f5fb74ddcc58c22b0
vpnchengo <charo.alvin@gmail.com>
2023-09-28 14:57:30 +0300

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+a function returning perimeter of island
+"""
+def island_perimeter(grid):
+    """the function"""
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4  # Each land cell contributes 4 to the perimeter
+
+                # Check adjacent cells and subtract for each adjacent land cell
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
