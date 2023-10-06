157e62b2c7b9c507d333668393614abf997bf30b
vpnchengo <charo.alvin@gmail.com>
2023-10-06 11:56:35 +0300

    update

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,27 +3,27 @@
 
 
 def isWinner(x, nums):
-    """Function that performs prime game"""
     if not nums or x < 1:
         return None
+
     n = max(nums)
-    fltr = [True for _ in range(max(n + 1, 2))]
+    primes_count = [0] * (max(n + 1, 2))
+
     for i in range(2, int(pow(n, 0.5)) + 1):
-        if not fltr[i]:
-            continue
-        for j in range(i * i, n + 1, i):
-            fltr[j] = False
-    fltr[0] = fltr[1] = False
-    a = 0
-    for i in range(len(fltr)):
-        if fltr[i]:
-            a += 1
-        fltr[i] = a
-    plyr1 = 0
-    for n in nums:
-        plyr1 += fltr[n] % 2 == 1
+        if primes_count[i] == 0:
+            for j in range(i * i, n + 1, i):
+                primes_count[j] = False
+
+    primes_count[0] = primes_count[1] = 0
+
+    for i in range(2, len(primes_count)):
+        primes_count[i] = primes_count[i - 1] + primes_count[i]
+
+    plyr1 = sum(primes_count[n] % 2 == 1 for n in nums)
+
     if plyr1 * 2 == len(nums):
         return None
-    if plyr1 * 2 > len(nums):
+    elif plyr1 * 2 > len(nums):
         return "Maria"
-    return "Ben"
+    else:
+        return "Ben"
